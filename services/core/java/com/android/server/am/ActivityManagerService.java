19888242bbd54aa82d37a247878988ff52ac124a
LibXZR <i@xzr.moe>
2022-09-26 01:48:24 +0300

    base: Add support for strict standby policy When dismissing from recents, this will force stop apps with restricted battery usage and put them into RESTRICTED standby bucket. Also all restrictions will be imposed even when device is charging.
    This can be useful to kill dirty vigorous Chinese apps.
    
    Change-Id: I8f075ef48c5775971e3e0b8b1ea843d70a2e44c4
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>
    
    Taken from here https://github.com/VoidUI/frameworks_base/commit/4b9d896d28062c2db64678742aa0fb8dc7b98f36

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -17316,4 +17316,11 @@ public class ActivityManagerService extends IActivityManager.Stub
             Trace.traceBegin(traceTag, methodName + subInfo);
         }
     }
+
+    boolean shouldSkipBootCompletedBroadcastForPackage(ApplicationInfo info) {
+        return mActivityTaskManager.mAppStandbyInternal.isStrictStandbyPolicyEnabled() &&
+                getAppOpsManager().checkOpNoThrow(
+                        AppOpsManager.OP_RUN_ANY_IN_BACKGROUND,
+                        info.uid, info.packageName) != AppOpsManager.MODE_ALLOWED;
+    }
 }
