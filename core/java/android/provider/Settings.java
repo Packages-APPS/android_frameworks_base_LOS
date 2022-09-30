c52b72b9903adfbf824dc8712c64a85fd2269015
Dmitrii <bankersenator@gmail.com>
2022-10-01 01:33:18 +0300

    SystemUI: Add a toggle for combined signal icons in status bar (1/2) * @jhonboy121: Use the new feature flag APIs and the flag state as a default value, otherwise there is no point in having a "toggle" if the feature flag is not enabled
    Change-Id: If869fe31e43f198c4ced8e872c5c060a524f6462
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5204,7 +5204,7 @@ public final class Settings {
          */
         @Readable
         public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-
+        
         /**
          * Whether or not to enable multiple audio focus.
          * When enabled, requires more management by user over application playback activity,
@@ -10336,6 +10336,11 @@ public final class Settings {
          */
         public static final String DEVICE_STATE_ROTATION_LOCK =
                 "device_state_rotation_lock";
+
+        /**
+         * @hide
+         */
+        public static final String SHOW_COMBINED_STATUS_BAR_SIGNAL_ICONS = "show_combined_status_bar_signal_icons";
 
         /**
          * These entries are considered common between the personal and the managed profile,
