813e50ebb43fcbdb53d6ed482ebafbe4059bf2ef
maxwen <max.weninger@gmail.com>
2022-10-14 22:19:16 +0300

    SystemUI: Add QS tile layout settings [1/2] @neobuddy89: * Removed unused configs and imports * De-omnify with TunerService and TileUtils
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5250,6 +5250,26 @@ public final class Settings {
         public static final String ENABLE_RIPPLE_EFFECT = "enable_ripple_effect";
 
         /**
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS_LANDSCAPE = "qs_layout_columns_landscape";
+
+        /**
+         * @hide
+         */
+        public static final String QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+        /**
+         * @hide
+         */
+        public static final String QS_TILE_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
+
+        /**
+         * @hide
+         */
+        public static final String QS_TILE_LABEL_HIDE = "qs_tile_label_hide";
+
+        /**
          * Arcane Idle Manager
          * @hide
          */
